99703f1e7d475e29077d25641e023600454371f5
Mike Binetsky <michael.binetsky@gmail.com>
2021-07-03 21:36:57 -0400

    Jacob helped me

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,16 +7,19 @@ pollpath = os.path.join("Resources", "election_data.csv")
 
 totalvotes = 0
 candidatelist = []
+votes = {}
 
 with open(pollpath) as pollfile:
     #This converts the file I opened into a dictionary
     pollreader = csv.DictReader(pollfile)
     for row in pollreader:
+        # I use this method instead of taking the length of a list since I don't have a list the length of the data. That's it.
         totalvotes += 1
         if row["Candidate"] not in candidatelist:
             candidatelist.append(row["Candidate"])
+            votes.update({row["Candidate"] : 0})
+        votes[row["Candidate"]] += 1
 
-
-print(f' Total Votes: {totalvotes}')
+print(f'Total Votes: {totalvotes}')
 for i in candidatelist:
-    print(i)
+    print(f'{i}: {votes[i]}')
