291666121e752b20feeebeaba5829e13d9694c12
Mike Binetsky <michael.binetsky@gmail.com>
2021-07-03 22:36:21 -0400

    Last code updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,62 +8,62 @@ import statistics
 bankpath = os.path.join("Resources", "budget_data.csv")
 
 # Same thing here. This will be increased in the loop to print later
-totalprofit = 0
+totalProfit = 0
 
 # This is an empty list that will be appended in the loop to calculate the monthly change and add that change to this list to do calculations on later
-monthlychange = []
-monthlylist = []
+monthlyChange = []
+monthlyList = []
 
 #  These two are here to help calculate the monthly change
-currentmonthamount = 0
+currentMonthAmount = 0
 # the lastmonthamount will start as "none" so that in the first iteration of the loop it won't count any change and disrupt the data
-lastmonthamont = None
+lastMonthAmont = None
 
 # First we open the file
 with open(bankpath) as bankfile:
     # This is converting the file from a csv into a dicitonary. I personally like dictionaries more than lists.
     bankreader = csv.DictReader(bankfile)
     for row in bankreader:
-        # First, we set the current month's profit and losses into the currentmonthamount to be used later
-        currentmonthamount = int(row['Profit/Losses'])
+        # First, we set the current month's profit and losses into the currentMonthAmount to be used later
+        currentMonthAmount = int(row['Profit/Losses'])
         # Then we iterate the monthly total by one to return later
         # The total profit starts at 0 and is added to this total profit to be returned later
-        totalprofit += currentmonthamount
+        totalProfit += currentMonthAmount
         # This if does a check to see if the last month amount is empty.
-        if lastmonthamont != None:
+        if lastMonthAmont != None:
             # Take the current month's amount minus the last month's amount to find the difference and add it to the monthly change list
-            monthlychange.append(currentmonthamount - lastmonthamont)
-            monthlylist.append(row['Date'])
+            monthlyChange.append(currentMonthAmount - lastMonthAmont)
+            monthlyList.append(row['Date'])
             # If the last month's profit/losses is empty, this gets skipped since there can be no change if there has been no change. Ya'know?
         #Finally, set the last month's amount to the current month's amount to be used in the next iteration of row
-        lastmonthamont = currentmonthamount
+        lastMonthAmont = currentMonthAmount
 
 # The monthly list is the number of months there was change. Since the only month without change was the first we take the length of the month list and add 1.
-monthtotal = len(monthlylist) + 1
+monthTotal = len(monthlyList) + 1
 
-print(f'Total Months = {monthtotal}')
-print(f'Total Profits = {totalprofit}')
-print(f' Average Monthly Change = {round(statistics.mean(monthlychange), 2)}')
+print(f'Total Months = {monthTotal}')
+print(f'Total Profits = {totalProfit}')
+print(f' Average Monthly Change = {round(statistics.mean(monthlyChange), 2)}')
 
 # Okay this one is a bit weird. 
-# First I set up two new variables: maxmonth and minmonth and set them both to None or Null
-maxmonth = None
-minmonth = None
-maxchange = max(monthlychange)
-minchange = min(monthlychange)
-# Let's start a loop to go through each item in monthlychange --> The list we added the changes from month to month
-for month in monthlychange:
-    # for each entry in my monthlychange list I check to see if it's the highest value in the entire list. 
+# First I set up two new variables: maxMonth and minMonth and set them both to None or Null
+maxMonth = None
+minMonth = None
+maxchange = max(monthlyChange)
+minchange = min(monthlyChange)
+# Let's start a loop to go through each item in monthlyChange --> The list we added the changes from month to month
+for month in monthlyChange:
+    # for each entry in my monthlyChange list I check to see if it's the highest value in the entire list. 
     if month == maxchange:
         # If it is, I grab the index of that value
-        maxmonth = monthlychange.index(month)
-    # for each entry in my monthlychange list I check to see if it's the highest value in the entire list. 
+        maxMonth = monthlyChange.index(month)
+    # for each entry in my monthlyChange list I check to see if it's the highest value in the entire list. 
     if month == minchange:
         # If it is, I grab the index of that value
-        minmonth = monthlychange.index(month)
+        minMonth = monthlyChange.index(month)
 
-print(f'Greatest Increase in profits: {monthlylist[maxmonth]} {maxchange}')
-print(f'Greatest Decrease in profits: {monthlylist[minmonth]} {minchange}')
+print(f'Greatest Increase in profits: {monthlyList[maxMonth]} {maxchange}')
+print(f'Greatest Decrease in profits: {monthlyList[minMonth]} {minchange}')
 
 # This is defining the outputh path for the new .txt file
 resultspath = os.path.join("Analysis", "bankresults.txt")
@@ -73,8 +73,8 @@ with open(resultspath, 'w') as results:
     # Here I write the output that I have pritned above. The /n at the end of the lines creates a new line in the text file
     results.write("Financial Analysis\n")
     results.write("------------------\n")
-    results.write(f"Total Months: {monthtotal}\n")
-    results.write(f"Total Profits/Losses: {totalprofit}\n")
-    results.write(f"Average Change: {round(statistics.mean(monthlychange), 2)}\n")
-    results.write(f'Greatest Increase in profits: {monthlylist[maxmonth]} {max(monthlychange)}\n')
-    results.write(f'Greatest Decrease in profits: {monthlylist[minmonth]} {min(monthlychange)}')
+    results.write(f"Total Months: {monthTotal}\n")
+    results.write(f"Total Profits/Losses: {totalProfit}\n")
+    results.write(f"Average Change: {round(statistics.mean(monthlyChange), 2)}\n")
+    results.write(f'Greatest Increase in profits: {monthlyList[maxMonth]} {max(monthlyChange)}\n')
+    results.write(f'Greatest Decrease in profits: {monthlyList[minMonth]} {min(monthlyChange)}')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,31 +9,43 @@ totalvotes = 0
 votes = {}
 
 with open(pollpath) as pollfile:
-    #This converts the file I opened into a dictionary
+    # This converts the file I opened into a dictionary
     pollreader = csv.DictReader(pollfile)
     for row in pollreader:
         # I use this method instead of taking the length of a list since I don't have a list the length of the data. That's it.
         totalvotes += 1
+        # This if statement checks if the current row's candidate is in our votes dictionary. 
+        # If they aren't, they are added to the dictionary with a starting vote of zero.
         if row["Candidate"] not in votes:
             votes.update({row["Candidate"] : 0})
+        # Then, I increment the value of that candidate's vote in the votes dictionary by one.
         votes[row["Candidate"]] += 1
 
 print(f'Total Votes: {totalvotes}')
+
+# I run a for loop through my votes dictionary to get the percentage of total votes for each candidate.
 for i in votes:
-    percentage = int(votes[i]) / totalvotes
+    # For each candidate in the votes dictionary, we get their votes out of the total votes
+    percentage = int(votes[i]) / totalvotes 
+    # Then mulitply by 100 to get the percentage
     percentage *= 100
+    # And round for good measure
     percentage = round(percentage, 0)
     print(f'{i}: {percentage}00% {votes[i]}')
 
+# For the sake of efficiency, I set this value before running my for loop
 highestvotes = max(votes.values())
 winnername = None
+# This goes through my votes dictionary to assign the winnername variable to the candidate with the most votes
 for name, vote in votes.items():
     if vote == highestvotes:
         winnername = name
-        print(f'Winner: {name}')
+        print(f'Winner: {winnername}')
 
+# Setting the output for the text file
 resultspath = os.path.join("Analysis", "pollresults.txt")
 
+# This writes the text file to show the same results as what is printed in the terminal with a bit more formatting
 with open(resultspath, 'w') as results:
     results.write("Election Results\n")
     results.write("----------------\n")
