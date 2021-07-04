36ba42d821a74a88208cc02a256500deee795bfc
Mike Binetsky <michael.binetsky@gmail.com>
2021-07-03 22:13:21 -0400

    I have completed both. Now I clean.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,7 +6,6 @@ import csv
 pollpath = os.path.join("Resources", "election_data.csv")
 
 totalvotes = 0
-candidatelist = []
 votes = {}
 
 with open(pollpath) as pollfile:
@@ -15,11 +14,35 @@ with open(pollpath) as pollfile:
     for row in pollreader:
         # I use this method instead of taking the length of a list since I don't have a list the length of the data. That's it.
         totalvotes += 1
-        if row["Candidate"] not in candidatelist:
-            candidatelist.append(row["Candidate"])
+        if row["Candidate"] not in votes:
             votes.update({row["Candidate"] : 0})
         votes[row["Candidate"]] += 1
 
 print(f'Total Votes: {totalvotes}')
-for i in candidatelist:
-    print(f'{i}: {votes[i]}')
+for i in votes:
+    percentage = int(votes[i]) / totalvotes
+    percentage *= 100
+    percentage = round(percentage, 0)
+    print(f'{i}: {percentage}00% {votes[i]}')
+
+highestvotes = max(votes.values())
+winnername = None
+for name, vote in votes.items():
+    if vote == highestvotes:
+        winnername = name
+        print(f'Winner: {name}')
+
+resultspath = os.path.join("Analysis", "pollresults.txt")
+
+with open(resultspath, 'w') as results:
+    results.write("Election Results\n")
+    results.write("----------------\n")
+    results.write(f'Total Votes: {totalvotes}\n')
+    results.write("----------------\n")
+    for i in votes:
+        percentage = int(votes[i]) / totalvotes
+        percentage *= 100
+        percentage = round(percentage, 0)
+        results.write(f'{i}: {percentage}00% {votes[i]}\n')
+    results.write("----------------\n")
+    results.write(f'Winner: {winnername}')
